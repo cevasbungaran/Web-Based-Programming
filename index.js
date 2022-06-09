0507f43046d689b9eff05ea8462439f03be31f2f
cevasbungaran <cevasbungaran@gmail.com>
2022-06-09 07:00:34 +0700

    benerin search

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 import express from 'express'
 import path from 'path'
+import mysql from 'mysql'
+import {fileURLToPath} from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
 const app = express();
 const PORT = 8082;
-
+const __dirname = path.dirname(__filename);
 app.set('view engine', 'ejs');
-//app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname+ '/public'));
 app.use(express.static(path.resolve('views')));
 
 app.listen(PORT, () =>{
     console.log(`Listening on port ${PORT}`);
 });
+//////////CONECT DB////////////
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'tubespbw'
+    });
 
+connection.connect(function(err) {
+    if (err) {
+          console.error('error connecting: ' + err.stack);
+          return;
+    }
+       
+        console.log('Connected to DB');
+});
+//////////////////
+
+
+
+
+///////////router//////////////////
 app.get('/', (req,res) => {
     res.render('landingpage');
 })
@@ -27,3 +52,52 @@ app.get('/homepage', (req,res) => {
 app.get('/mythreads', (req,res) => {
     res.render('mythreads');
 })
+app.post('/signup', (req,res) => {
+    var sql = `INSERT INTO users (name, password, joined_date) VALUES ('${req.body.username}','${req.body.password}','${new Date().toISOString().slice(0, 19).replace('T', ' ')}')`
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+      });
+      res.redirect("/login")
+})
+
+app.get('/logincheck', (req,res) => {
+    var sql = `(SELECT count(users.id) as jumlah from users where name='${req.query.username}' and password='${req.query.password}')`
+    connection.query(sql, function (err, result) {
+        if (err) throw err;;
+        if(result[0].jumlah>0){
+            res.redirect("/homepage")
+        }
+        else{
+            res.redirect("/login")
+        }
+      }); 
+})
+
+app.get('/homepageget/*', (req,res) => {
+    var value=req.params[0]
+    if (req.params[0]==undefined){
+        value='';
+    }
+    var sql = `(SELECT threads.title as title, users.name as usrname, thread_categories.name as category, threads.created_date as creat from threads inner join users on threads.author_id=users.id inner join thread_categories on thread_categories.id=threads.category_id where title like '%${value}%')`
+    connection.query(sql, function (err, result) {
+        if (err) throw err;;
+        res.send(result)
+      }); 
+})
+
+app.get('/myhomepageget/*', (req,res) => {
+    var par=req.params[0]
+    const arr=par.split("/")
+    var name=arr[0]
+    var value=arr[1]
+   if (req.params[0]==undefined){
+        value='';
+    }
+    var sql = `(SELECT threads.title as title, users.name as usrname, thread_categories.name as category, threads.created_date as creat from threads inner join users on threads.author_id=users.id inner join thread_categories on thread_categories.id=threads.category_id where title like '%${value}%' and users.name='${name}')`
+    connection.query(sql, function (err, result) {
+        if (err) throw err;;
+        res.send(result)
+      }); 
+})
+/////////////////////
