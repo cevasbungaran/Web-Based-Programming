89b5cc77048dddd94a275f70abfac106118cbefe
cevasbungaran <cevasbungaran@gmail.com>
2022-06-10 11:10:48 +0700

    benerin local storage di mythread

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.get('/mythreads', (req,res) => {
     res.render('mythreads');
 })
 app.post('/signup', (req,res) => {
-    var sql = `INSERT INTO users (name, password, joined_date) VALUES ('${req.body.username}','${req.body.password}','${new Date().toISOString().slice(0, 19).replace('T', ' ')}')`
+    let sql = `INSERT INTO users (name, password, joined_date) VALUES ('${req.body.username}','${req.body.password}','${new Date().toISOString().slice(0, 19).replace('T', ' ')}')`
     connection.query(sql, function (err, result) {
         if (err) throw err;
         console.log("1 record inserted");
@@ -62,7 +62,7 @@ app.post('/signup', (req,res) => {
 })
 
 app.get('/logincheck', (req,res) => {
-    var sql = `(SELECT count(users.id) as jumlah from users where name='${req.query.username}' and password='${req.query.password}')`
+    let sql = `(SELECT count(users.id) as jumlah from users where name='${req.query.username}' and password='${req.query.password}')`
     connection.query(sql, function (err, result) {
         if (err) throw err;;
         if(result[0].jumlah>0){
@@ -75,11 +75,11 @@ app.get('/logincheck', (req,res) => {
 })
 
 app.get('/homepageget/*', (req,res) => {
-    var value=req.params[0]
+    let value=req.params[0]
     if (req.params[0]==undefined){
         value='';
     }
-    var sql = `(SELECT threads.title as title, users.name as usrname, thread_categories.name as category, threads.created_date as creat from threads inner join users on threads.author_id=users.id inner join thread_categories on thread_categories.id=threads.category_id where title like '%${value}%')`
+    let sql = `(SELECT threads.title as title, users.name as usrname, thread_categories.name as category, threads.created_date as creat from threads inner join users on threads.author_id=users.id inner join thread_categories on thread_categories.id=threads.category_id where title like '%${value}%')`
     connection.query(sql, function (err, result) {
         if (err) throw err;;
         res.send(result)
@@ -87,14 +87,14 @@ app.get('/homepageget/*', (req,res) => {
 })
 
 app.get('/myhomepageget/*', (req,res) => {
-    var par=req.params[0]
+    let par=req.params[0]
     const arr=par.split("/")
-    var name=arr[0]
-    var value=arr[1]
+    let name=arr[0]
+    let value=arr[1]
    if (req.params[0]==undefined){
         value='';
     }
-    var sql = `(SELECT threads.title as title, users.name as usrname, thread_categories.name as category, threads.created_date as creat from threads inner join users on threads.author_id=users.id inner join thread_categories on thread_categories.id=threads.category_id where title like '%${value}%' and users.name='${name}')`
+    let sql = `(SELECT threads.title as title, users.name as usrname, thread_categories.name as category, threads.created_date as creat from threads inner join users on threads.author_id=users.id inner join thread_categories on thread_categories.id=threads.category_id where title like '%${value}%' and users.name='${name}')`
     connection.query(sql, function (err, result) {
         if (err) throw err;;
         res.send(result)
