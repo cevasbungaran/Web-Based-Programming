d1507edf17000ea6b0aa8cbf8684d5fac8af8760
cevasbungaran <cevasbungaran@gmail.com>
2022-06-27 21:10:12 +0700

    final hapus komen

--- public/mythread.js
+++ public/mythread.js
@@ -73,7 +73,3 @@ function show(data, i) {
 
     document.getElementById("threadArray").innerHTML = tab;
 }
-
-// const paginate = (array, pageSize, pageNumber) => {
-//     return array.slice((pageNumber - 1) * pageSize, pageNumber * pageSize);
-// }
